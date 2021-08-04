d4b7ecd79c3c3ab7cca379ae5eeddee74f35e72b
Hans Schedel <hans.haugusth@gmail.com>
2021-08-04 17:57:52 -0300

    fix: _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,7 +41,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-br">
-        <head />
+        <Head />
         <body>
           <Main />
           <NextScript />
