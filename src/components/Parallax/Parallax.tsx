4d6e5aded54cb4b903916af08fe3b61387d77681
Hans Schedel <hans.haugusth@gmail.com>
2021-08-07 04:24:58 -0300

    feat: add parallax component

--- src/components/Parallax/Parallax.tsx
+++ src/components/Parallax/Parallax.tsx
@@ -1,29 +1,39 @@
+import { Parallax } from 'react-parallax'
 import {
-  ParallaxBackgroundImage,
+  ParallaxContent,
+  ParallaxIcon,
+  ParallaxName,
+  ParallaxOffice,
+  ParallaxPhrase,
   ParallaxWrapper,
 } from '@/styles/components/Parallax/Parallax'
-import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons'
 
 export default function ProformParallax(): JSX.Element {
-  const [offsetY, setOffsetY] = useState(0)
-
-  function handleScroll() {
-    setOffsetY(window.pageYOffset)
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-
-    return window.removeEventListener('scroll', handleScroll)
-  })
-
   return (
-    <ParallaxWrapper>
-      <ParallaxBackgroundImage
-        src="assets/banner_3.jpg"
-        alt="constructions"
-        offset={offsetY}
-      />
-    </ParallaxWrapper>
+    <Parallax
+      blur={{ min: -25, max: 25 }}
+      bgImage="assets/banner_3.jpg"
+      bgImageAlt="constructions"
+      strength={350}
+      style={{ opacity: 0.8 }}
+    >
+      <ParallaxWrapper>
+        <ParallaxContent>
+          <ParallaxPhrase>
+            <ParallaxIcon>
+              <FontAwesomeIcon icon={faQuoteLeft} />
+            </ParallaxIcon>
+            Só se faz progresso quando se pensa que algo mais pode ser feito.
+            <ParallaxIcon className="end">
+              <FontAwesomeIcon icon={faQuoteRight} />
+            </ParallaxIcon>
+          </ParallaxPhrase>
+          <ParallaxName>Guillermo Marconi</ParallaxName>
+          <ParallaxOffice>Inventor e físico italiano</ParallaxOffice>
+        </ParallaxContent>
+      </ParallaxWrapper>
+    </Parallax>
   )
 }
--- src/styles/components/Parallax/Parallax.ts
+++ src/styles/components/Parallax/Parallax.ts
@@ -1,46 +1,78 @@
-import styled, { css } from 'styled-components'
-import Image from '@/components/Image/Image'
+import styled from 'styled-components'
+import { Theme } from '@/styles/Theme'
 import { media } from '@/styles/GlobalStyles'
 
-interface IParallaxProps {
-  offset: number
-}
-
-function parallaxEffect({ offset }: IParallaxProps) {
-  const pixels = `${offset * 0.5}px`
-
-  return css`
-    transform: translateY(${pixels});
-  `
-}
-
 export const ParallaxWrapper = styled.div`
+  align-items: center;
   display: flex;
-  height: 50vh;
-  overflow: hidden;
+  height: 75vh;
+  justify-content: center;
   width: 100%;
-`
-
-export const ParallaxBackgroundImage = styled(Image)<IParallaxProps>`
-  ${parallaxEffect};
-  -moz-transition: position 10s;
-  -ms-transition: position 10s;
-  -o-transition: position 10s;
-  -webkit-transition: all 0.5s ease;
-  height: 100%;
-  object-fit: cover;
-  opacity: 0.8;
-  position: absolute;
-  transition: all 0.5s ease;
-  width: 100%;
-  z-index: -5;
-
-  ${media.tablet} {
-    margin-top: -115%;
-  }
 `
 
 export const ParallaxContent = styled.div`
-  position: relative;
-  width: 80%;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+
+  ${media.smallTablet} {
+    width: 80%;
+  }
+`
+
+export const ParallaxIcon = styled.div`
+  font-size: 32px;
+  height: max-content;
+  margin: 0 15px;
+  opacity: 0.7;
+  top: 0;
+
+  &.end {
+    align-self: end;
+  }
+
+  ${media.smallTablet} {
+    font-size: 24px;
+    line-height: 32px;
+  }
+`
+
+export const ParallaxPhrase = styled.div`
+  color: ${Theme.Black};
+  display: flex;
+  font-size: 46px;
+  font-style: italic;
+  justify-content: space-around;
+  line-height: 63px;
+  text-align: center;
+
+  ${media.smallTablet} {
+    font-size: 24px;
+    line-height: 32px;
+  }
+`
+
+export const ParallaxName = styled.div`
+  color: ${Theme.ProformGreen};
+  font-size: 32px;
+  font-weight: bold;
+  line-height: 43px;
+  margin-top: 38px;
+
+  ${media.smallTablet} {
+    font-size: 18px;
+    line-height: 24px;
+  }
+`
+
+export const ParallaxOffice = styled.div`
+  font-size: 24px;
+  line-height: 32px;
+  margin-top: 10px;
+
+  ${media.smallTablet} {
+    font-size: 14px;
+    line-height: 19px;
+  }
 `
