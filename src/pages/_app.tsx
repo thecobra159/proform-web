37bcfea65b6371aeca99f6b2342a15e459aac505
Hans Schedel <hans.haugusth@gmail.com>
2021-08-13 00:23:04 -0300

    feat: add libs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import GlobalStyles from '@/styles/GlobalStyles'
 import Header from '@/components/Header/Header'
 import { ParallaxProvider } from 'react-scroll-parallax'
 import Footer from '@/components/Footer/Footer'
+import { ToastContainer } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
@@ -11,6 +14,17 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <ParallaxProvider>
         <Header />
         <Component {...pageProps} />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </ParallaxProvider>
       <Footer />
     </>
